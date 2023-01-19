00c87374c91bfde7a4bdcdc4ec89c513ad44b1e3
wwwbijay <link2bj@gmail.com>
2023-01-19 22:04:36 +0545

    packages added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,27 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: parseInt(process.env.DATABASE_PORT, 10) || 3306,
+      username: process.env.DATABASE_USERNAME,
+      password: '',
+      database: 'test',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix(process.env.GLOBAL_PREFIX);
   const config = new DocumentBuilder()
-    .setTitle('VMS API')
-    .setDescription('Vehicle Management System Rest API.')
-    .setVersion('1.0')
+    .setTitle(process.env.SWAGGER_TITLE)
+    .setDescription(process.env.SWAGGER_DESC)
+    .setVersion(process.env.SWAGGER_VER)
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3001);
 }
