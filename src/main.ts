ea1948ef72a0e921aecf69c9dc548e49b81c8041
wwwbijay <link2bj@gmail.com>
2023-01-19 21:16:21 +0545

    initial

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  const config = new DocumentBuilder()
+    .setTitle('VMS API')
+    .setDescription('Vehicle Management System Rest API.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3001);
+}
+bootstrap();
