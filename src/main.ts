1ce269c02df5ff49a5558c0cfe7699a11c790300
MyPayNepal <link2bj@gmail.com>
2023-01-22 12:45:45 +0545

    helmet added for security

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  
   app.setGlobalPrefix(process.env.GLOBAL_PREFIX);
   const config = new DocumentBuilder()
     .setTitle(process.env.SWAGGER_TITLE)
